cd6174386dd07b8bc21459d2f409a731c3d8ba0d
Guy Gubnitsky <g.gubnitsky@gmail.com>
2022-11-10 11:57:15 +0100

    added other whale functionality

--- Main_server.m
+++ Main_server.m
@@ -1,17 +1,20 @@
 clear all; clc;
 
-Program_folder=pwd;                                     % current folder
-files = dir('SW*.flac');
-% files = dir('AMAR*.flac');
-% files = dir('10M*.wav');
-
+Program_folder=pwd;
+files = dir('twowhales*.flac');
+ 
     %% Pick Detector
 
     Detector_flag=0; % 1- apply coda detector | 0- apply echolocation clicks detector
     Plot_flag=1;     % 1- show detection figures | 0- dont show
+    Tag_flag=1;    % 1- apply transient detector suitable for Dtags | apply IPI detector customized for bouy data
+    Error_flag=length(files);
 
-    Other_whale_Detector_flag=0; %1- apply other whale detector | 0- disable other whale detector
-
+    %% Check filename:
+    if Error_flag==0
+        error('File not found. Please check the file name (line 4)');        
+    end
+        
     %% Determine operational parameters (Global parameters)
 
     F_low_coda = 3e3;
@@ -33,13 +36,12 @@ files = dir('SW*.flac');
         
     for file_ind=1:length(files)
 
-        cd(Data_folder);
         filename=files(file_ind).name;
+        filesave=[Program_folder '\' filename '.xls'];
         Audio_name=[strfind(filename, 'flac') strfind(filename, 'wav')];
         if ~isempty(Audio_name)
             Timer=0;
             [y,Fs] = audioread(filename);                 % load recordings
-            cd(Program_folder);
             Y=y(:,1);                                     % Choose chanel one from the WRU
             if Fs < FsAnalyze
                 S_factor = 1;
@@ -54,20 +56,27 @@ files = dir('SW*.flac');
             T=F_ds*T_sec;                                 % Define duration of window for analysis in samples
             T_raw=Fs*T_sec;
             NOI=floor(File_duration/T_sec);               % Calculate the number of windows in the current recording
-            buffer_index=0; Gather_TOA=[]; 
+            buffer_index=0; 
+            Gather_TOA=[]; 
+            TOA_other_whale=[];
 
             for Buffer_ind=1:NOI
                 Y_filtered=bandpass(Y_decimated(int32((Buffer_ind-1)*T+1):int32((Buffer_ind-1)*T+T)),[F_low, F_high],F_ds);     % Aply band pass filter and extract buffer                             
-                TOA=Run_Detector_server(Y_filtered,Fs,F_ds,Detector_flag,Plot_flag);  % Run detection
-                Gather_TOA=[Gather_TOA (Buffer_ind-1)*T_sec+TOA];
+                if Tag_flag
+                    [~,TOA,TOA_other]=Run_Detector_server(Y_filtered,Fs,F_ds,Detector_flag,Plot_flag,Tag_flag);  % Run detection
+                else
+                    [TOA,~,~]=Run_Detector_server(Y_filtered,Fs,F_ds,Detector_flag,Plot_flag);  % Run detection                    
+                end
+                Gather_TOA=[Gather_TOA (Buffer_ind-1)*T_sec+sort(TOA)];
+                TOA_other_whale=[TOA_other_whale (Buffer_ind-1)*T_sec+sort(TOA_other)];
             end
             
-            if Other_whale_Detector_flag
-                DetectOtherWhale=find_whale_neighbours(Y,Gather_TOA,Fs); % Run 'other whale' detector
-                if ~isempty(DetectOtherWhale)
-                    writematrix(DetectOtherWhale',filesave); % Save time of arrivals of the detected 'other whales clicks'
-                end
-            elseif ~isempty(Gather_TOA)               
+            if Tag_flag
+                  filesave=[Program_folder '\' 'Tag_detected_' filename '.xls'];
+                  writematrix(Gather_TOA',filesave);
+                  filesave=[Program_folder '\' 'Other_whale_detected_' filename '.xls'];
+                  writematrix(TOA_other_whale',filesave);                                                    
+            else                
                  writematrix(Gather_TOA',filesave);  % Save time of arrivals of the detected clicks                                                              
             end 
         end
